2aef50396fd9be83caa7932725205358bc71ec3e
Jim <jim4m80@gmail.com>
2021-04-29 21:11:00 -0500

    changed to classification

--- main.py
+++ main.py
@@ -5,23 +5,18 @@ Created on Wed Apr 21 14:11:48 2021
 @author: moore
 """
 
-import os
 import glob
 
-import IPython.display as ipd
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 import sklearn as skl
 import sklearn.utils, sklearn.preprocessing, sklearn.decomposition, sklearn.svm
+from sklearn.neural_network import MLPClassifier
+from joblib import dump, load
 import librosa
 import librosa.display
+
 import utils
-import json
-
-
-
 
 #if the song is shorter than the target_duration in seconds, it will pad with zeros
 def path_to_mfcc(filename, target_duration=31):
@@ -65,8 +60,138 @@ def calc_and_save_mfccs(save_path, audio_dir = 'data\\fma_small\\'):
             save_mfccs(mfcc, save_path + folder)
             print("Finished folder " + folder)
             
+def load_transform_mfccs(path):
+    mfccs = np.load(path)
+    fmfccs = []
+    for m in mfccs:
+        fmfccs.append(m.flatten())
+    return fmfccs
 
-calc_and_save_mfccs('data\\fma_small_mfcc\\')
+def get_listens(folder):
+    paths = glob.glob(folder + "\\*.wav")
+    l = []
+    for path in paths:
+        l.append(all_listens[int(path[path.rfind('\\')+1:-4])])
+    return l
+
+#4012 songs out of 8000 with at least 2500 plays
+def get_listen_categories(folder, cutoff=2500):
+    paths = glob.glob(folder + "\\*.wav")
+    l = []
+    overcutoff = 0
+    undercutoff = 0
+    for path in paths:
+        value = 1 if all_listens[int(path[path.rfind('\\')+1:-4])] >= cutoff else 0
+        l.append(value)
+        overcutoff += value
+        undercutoff += 1-value
+    #print("P(under cutoff): " + ("%0.3f" % (1-overcutoff/total))+ ", P(over cutoff): " + ("%0.3f" % (overcutoff/total)))
+    return (l, overcutoff, undercutoff)
+
+def get_train_test_sets(test_interval=3,folder_ids=range(0,156),mfcc_folder='data\\fma_small_mfcc\\', audio_folder='data\\fma_small\\', cutoff=2500):
+    X_trn = []
+    y_trn = []
+    X_test = []
+    y_test = []
+    positives = 0
+    negatives = 0
+    for folderid in folder_ids:
+        foldername = "%03d" % folderid
+        fmfccs = load_transform_mfccs('data\\fma_small_mfcc\\' + foldername + '.npy') 
+        listens, pos, neg = get_listen_categories('data\\fma_small\\' + foldername,cutoff)
+        positives += pos
+        negatives += neg
+        for i in range(0,len(listens)):
+            if i % test_interval == 0:
+                X_test.append(fmfccs[i])
+                y_test.append(listens[i])
+            else:
+                X_trn.append(fmfccs[i])
+                y_trn.append(listens[i])
+                
+    return (np.array(X_trn),np.array(y_trn),np.array(X_test),np.array(y_test))
+
+def train(model, X_trn, y_trn):
+    model.fit(X_trn,y_trn)
+    return test(model,X_trn,y_trn)
+
+def test(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    return skl.metrics.confusion_matrix(y_test, y_pred)
+
+def avg_confusion(confusion_matrix):
+    tn, fp, fn, tp = confusion_matrix.ravel()
+    acc = (tn+tp)/(tn+fp+fn+tp)
+    return acc
+def print_confusion(confusion_matrix):
+    tn, fp, fn, tp = confusion_matrix.ravel()
+    print("true positives: " + str(tp) + ", true negatives: " + str(tn))
+    print("false positives: " + str(fp) + ", false negatives: " + str(fn))
+    acc = (tn+tp)/(tn+fp+fn+tp)
+    print("accuracy: " + ("%0.4f" % acc))
+    return acc
+
+def save_model(model, path):
+    dump(model, path)
+
+def create_test(iteration_min, iteration_max, iteration_step, save_path, hidden_sizes = (1000,500,100)):
+    accs_trn = []
+    accs_test = []
+    confusions = []
+    classifier = MLPClassifier(hidden_layer_sizes=hidden_sizes, random_state=1, max_iter=iteration_step, warm_start=True)
+    X_trn,y_trn,X_test,y_test = get_train_test_sets(folder_ids=range(0,156))
+    
+    for it in range(iteration_min,iteration_max,iteration_step):
+        #print("training with max iter=%d" % it)
+        acc_trn = avg_confusion(train(classifier,X_trn,y_trn))
+        print("testing with max iter=%d" % it)
+        conf = test(classifier,X_test,y_test)
+        confusions.append(conf)
+        acc_test = avg_confusion(conf)
+        accs_trn.append(acc_trn)
+        accs_test.append(acc_test)
+        print()
+        #save_model(classifier, save_path + str(hidden_sizes) + str(it) + ' epochs.joblib')
+    return (classifier, accs_trn,accs_test, confusions)
+
+
+all_tracks = utils.load('data/fma_metadata/tracks.csv')
+all_listens = all_tracks[all_tracks['set', 'subset'] <= 'small'][('track', 'listens')]
+
+it_min =    20
+it_max=     2000
+it_step =   20
+
+train_epochs = range(it_min, it_max, it_step)
+model, accs_trn, accs_test, confusions = create_test(it_min,it_max,it_step, 'data\\models\\')
+
+print()
+print('done')
+print()
+max_test_i = np.argmax(accs_test)
+print('maximum test accurracy: %.3f with %d epochs' % (accs_test[max_test_i], train_epochs[max_test_i]))
+print_confusion(confusions[max_test_i])
+plt.show()
+plt.figure()
+plt.xlim([0,it_max])
+plt.ylim([0,1])
+
+plt.plot(train_epochs, accs_trn, 'bo-', linewidth=2, label='train accuracy')
+plt.plot(train_epochs, accs_test, 'go-', linewidth=2, label='test accuracy')
+
+plt.legend(bbox_to_anchor=(1, 1))
+plt.xlabel('Training epochs', fontsize=16)
+plt.ylabel('Accuracy', fontsize=16)
+
+
+#save_model(regressor, 'data\\MLPregressor.joblib')
+#print('model saved!')
+#loaded = load('data\\MLPregressor.joblib')
+
+#print(test_folder(loaded,1))
+
+
+
 # ## 4 Audio
 # 
 # You can load the waveform and listen to audio in the notebook itself.
